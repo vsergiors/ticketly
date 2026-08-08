0c0fb4ec32471ea9a0e7a4a0c825bedf82a33ec9
vsergiors <vsergiobusiness@gmail.com>
2026-08-08 19:07:48 +0200

    Delete src directory

--- src/commands/claim.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { claimTicket } = require('../handlers/ticketHandlers');
-
-module.exports = {
-  data: new SlashCommandBuilder().setName('claim').setDescription('Reclama el ticket actual'),
-  async execute(interaction) {
-    await claimTicket(interaction);
-  },
-};
--- src/events/guildDelete.js
+++
@@ -1,9 +0,0 @@
-const supabase = require('../lib/supabase');
-
-module.exports = {
-  name: 'guildDelete',
-  async execute(guild) {
-    // no borramos datos, solo lo dejamos constar; puedes cambiar a delete si prefieres limpiar
-    console.log(`➖ Bot expulsado de ${guild.name} (${guild.id})`);
-  },
-};
