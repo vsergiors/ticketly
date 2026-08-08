09fd4b66c06c19e86e93cacec06b819b58e66d28
vsergiors <vsergiobusiness@gmail.com>
2026-08-08 12:57:12 +0200

    Add files via upload

---
+++ src/lib/supabase.js
@@ -0,0 +1,10 @@
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  { auth: { persistSession: false } }
+);
+
+module.exports = supabase;
